37216f823d90288cf8cfa44b16045b4666d006e2
marvelxcodes <ramakrishnan0046@gmail.com>
2023-01-22 11:57:14 +0530

    Added more quotes

--- quotes.ts
+++ quotes.ts
@@ -5,7 +5,7 @@ export type QuoteType = {
 
 const quotes: QuoteType[] = [
 	{
-		quote: `Stay hungry. Stay foolish.`,
+		quote: `Stay hungry. Stay foolish`,
 		by: "Steve Jobs",
 	},
 	{
@@ -13,8 +13,40 @@ const quotes: QuoteType[] = [
 		by: "Steve Jobs",
 	},
 	{
-		quote: "We’re here to put a dent in the universe. Otherwise why else even be here?",
+		quote: `We're here to put a dent in the universe. Otherwise why else even be here?`,
 		by: "Steve Jobs"
+	},
+	{
+		quote: `The greatest glory in living lies not in never falling, but in rising every time we fall`,
+		by: "Nelson Mandela"
+	},
+	{
+		quote: `The way to get started is to quit talking and begin doing`,
+		by: "Walt Disney"
+	},
+	{
+		quote: `Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma - which is living with the results of other people's thinking`,
+		by: "Steve Jobs"
+	},
+	{
+		quote: `If life were predictable it would cease to be life, and be without flavor`,
+		by: "Eleanor Roosevelt"
+	},
+	{
+		quote: `The way to get started is to quit talking and begin doing`,
+		by: "Eleanor Roosevelt"
+	},
+	{
+		quote: `If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough`,
+		by: "Oprah Winfrey"
+	},
+	{
+		quote: `If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success`,
+		by: "James Cameron"
+	},
+	{
+		quote: `Life is what happens when you're busy making other plans`,
+		by: "John Lennon"
 	}
 ];
 
