e89415262eec78b06b99956ea7f265c38222dc20
marvelxcodes <ramakrishnan0046@gmail.com>
2023-01-22 12:04:43 +0530

    Added More Quotes

--- quotes.ts
+++ quotes.ts
@@ -14,40 +14,104 @@ const quotes: QuoteType[] = [
 	},
 	{
 		quote: `We're here to put a dent in the universe. Otherwise why else even be here?`,
-		by: "Steve Jobs"
+		by: "Steve Jobs",
 	},
 	{
 		quote: `The greatest glory in living lies not in never falling, but in rising every time we fall`,
-		by: "Nelson Mandela"
+		by: "Nelson Mandela",
 	},
 	{
 		quote: `The way to get started is to quit talking and begin doing`,
-		by: "Walt Disney"
+		by: "Walt Disney",
 	},
 	{
 		quote: `Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma - which is living with the results of other people's thinking`,
-		by: "Steve Jobs"
+		by: "Steve Jobs",
 	},
 	{
 		quote: `If life were predictable it would cease to be life, and be without flavor`,
-		by: "Eleanor Roosevelt"
+		by: "Eleanor Roosevelt",
 	},
 	{
 		quote: `The way to get started is to quit talking and begin doing`,
-		by: "Eleanor Roosevelt"
+		by: "Eleanor Roosevelt",
 	},
 	{
 		quote: `If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough`,
-		by: "Oprah Winfrey"
+		by: "Oprah Winfrey",
 	},
 	{
 		quote: `If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success`,
-		by: "James Cameron"
+		by: "James Cameron",
 	},
 	{
 		quote: `Life is what happens when you're busy making other plans`,
-		by: "John Lennon"
-	}
+		by: "John Lennon",
+	},
+	{
+		quote: `Spread love everywhere you go. Let no one ever come to you without leaving happier`,
+		by: "Mother Teresa",
+	},
+	{
+		quote: `When you reach the end of your rope, tie a knot in it and hang on`,
+		by: "Franklin D. Roosevelt",
+	},
+	{
+		quote: `Always remember that you are absolutely unique. Just like everyone else`,
+		by: "Margaret Mead",
+	},
+	{
+		quote: `Don't judge each day by the harvest you reap but by the seeds that you plant`,
+		by: "Robert Louis Stevenson",
+	},
+	{
+		quote: `The future belongs to those who believe in the beauty of their dreams`,
+		by: "Eleanor Roosevelt",
+	},
+	{
+		quote: `Tell me and I forget. Teach me and I remember. Involve me and I learn`,
+		by: "Benjamin Franklin",
+	},
+	{
+		quote: `The best and most beautiful things in the world cannot be seen or even touched — they must be felt with the heart`,
+		by: "Helen Keller",
+	},
+	{
+		quote: `It is during our darkest moments that we must focus to see the light`,
+		by: "Aristotle",
+	},
+	{
+		quote: `Whoever is happy will make others happy too`,
+		by: "Anne Frank",
+	},
+	{
+		quote: `Do not go where the path may lead, go instead where there is no path and leave a trail`,
+		by: "Ralph Waldo Emerson",
+	},
+	{
+		quote: `Spread love everywhere you go. Let no one ever come to you without leaving happier`,
+		by: "Mother Teresa",
+	},
+	{
+		quote: `When you reach the end of your rope, tie a knot in it and hang on`,
+		by: "Franklin D. Roosevelt",
+	},
+	{
+		quote: `Always remember that you are absolutely unique. Just like everyone else`,
+		by: "Margaret Mead",
+	},
+	{
+		quote: `Don't judge each day by the harvest you reap but by the seeds that you plant`,
+		by: "Robert Louis Stevenson",
+	},
+	{
+		quote: `Tell me and I forget. Teach me and I remember. Involve me and I learn`,
+		by: "Benjamin Franklin",
+	},
+	{
+		quote: `The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart`,
+		by: "Helen Keller",
+	},
 ];
 
 export default quotes;
